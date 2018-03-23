1c528be4e3ac86d134b136fac3d9c458ab3be169
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-23 13:10:16 -0700

    Added Text Analytics & Emoji Services

---
+++ TextMood.Functions/Services/TextAnalysisServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.ProjectOxford.Text.Core;
+using Microsoft.ProjectOxford.Text.Sentiment;
+
+namespace TextMood.Functions
+{
+	static class TextAnalysisServices
+	{          
+		readonly static Lazy<SentimentClient> _sentimentClientHolder = new Lazy<SentimentClient>(() => new SentimentClient(CognitiveServicesConstants.TextSentimentAPIKey));
+
+		static SentimentClient SentimentClient => _sentimentClientHolder.Value;
+
+		public static async Task<float?> GetSentiment(string text)
+		{
+			var sentimentDocument = new SentimentDocument { Id = "1", Text = text };
+			var sentimentRequest = new SentimentRequest { Documents = new List<IDocument> { { sentimentDocument } } };
+
+			var sentimentResults = await SentimentClient.GetSentimentAsync(sentimentRequest);
+			var documentResult = sentimentResults.Documents.FirstOrDefault();
+
+			return documentResult?.Score;
+		}
+	}
+}
