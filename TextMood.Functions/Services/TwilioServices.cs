6f91af09710194187d240f84fd8162434c0bc583
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-25 13:19:38 -0700

    Added Twilio Service

---
+++ TextMood.Functions/Services/TwilioServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.Azure.WebJobs.Host;
+
+using Twilio.TwiML;
+
+namespace TextMood.Functions
+{
+    public static class TwilioServices
+    {
+		public static string GetTextMessageBody(string httpRequestBody, TraceWriter log)
+        {
+            var formValues = httpRequestBody?.Split('&')
+                                ?.Select(value => value.Split('='))
+                                ?.ToDictionary(pair => Uri.UnescapeDataString(pair[0]).Replace("+", " "),
+                                              pair => Uri.UnescapeDataString(pair[1]).Replace("+", " "));
+
+            foreach (var value in formValues)
+                log.Info($"Key: {value.Key}, Value: {value.Value}");
+
+            var textMessageKeyValuePair = formValues.Where(x => x.Key?.ToUpper()?.Equals("BODY") ?? false)?.FirstOrDefault();
+
+            return textMessageKeyValuePair?.Value;
+        }
+
+        public static string CreateTwilioResponse(string message)
+		{
+			var response = new MessagingResponse().Message(message);
+
+			return response.ToString().Replace("utf-16", "utf-8");
+		}
+    }
+}
