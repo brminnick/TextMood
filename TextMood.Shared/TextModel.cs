61f9ecf55e615e951986682bbcbec8e4a7698d6a
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-24 20:04:13 -0700

    Added TextMoodDatabase Class

---
+++ TextMood.Shared/TextModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+
+namespace TextMood.Shared
+{
+    public class TextModel
+    {
+		[Column(Name = nameof(Id), IsPrimaryKey = true, CanBeNull = false, UpdateCheck = UpdateCheck.Never)]
+		public string Id { get; set; }
+
+		[Column(Name = nameof(Text), IsPrimaryKey = false, CanBeNull = false, UpdateCheck = UpdateCheck.Never)]
+		public string Text { get; set; }
+
+		[Column(Name = nameof(CreatedAt), IsPrimaryKey = false, CanBeNull = false, UpdateCheck = UpdateCheck.Never)]
+		public DateTimeOffset CreatedAt { get; set; }
+
+		[Column(Name = nameof(UpdatedAt), IsPrimaryKey = false, CanBeNull = false, UpdateCheck = UpdateCheck.Never)]
+		public DateTimeOffset UpdatedAt { get; set; }
+
+		[Column(Name = nameof(IsDeleted), IsPrimaryKey = false, CanBeNull = false, UpdateCheck = UpdateCheck.Never)]
+		public bool IsDeleted { get; set; }
+    }
+}
