f3bec421b7494195550b66b37555ace21fcaf795
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-29 11:03:04 -0700

    Shortened Text string that is saved to database

--- TextMood/ViewModels/TextResultsListViewModel.cs
+++ TextMood/ViewModels/TextResultsListViewModel.cs
@@ -47,7 +47,11 @@ namespace TextMood
 		#endregion
 
 		#region Methods
-		Task ExecutePullToRefreshCommand() => UpdateTextResultsList();
+		async Task ExecutePullToRefreshCommand()
+		{
+			await UpdateTextResultsList().ConfigureAwait(false);
+			SetTextResultsListBackgroundColor(TextList);
+		}
 
 		async Task UpdateTextResultsList()
 		{
@@ -57,10 +61,8 @@ namespace TextMood
 
 				var textMoodList = await TextResultsService.GetTextModels().ConfigureAwait(false);
 				var recentTextMoodList = TextModelServices.GetRecentTextModels(new List<ITextMoodModel>(textMoodList), TimeSpan.FromHours(1));
-                
-				TextList = recentTextMoodList.OrderByDescending(x => x.CreatedAt).ToList();
 
-				SetTextResultsListBackgroundColor(recentTextMoodList);
+				TextList = recentTextMoodList.OrderByDescending(x => x.CreatedAt).ToList();
 			}
 			catch (Exception e)
 			{
@@ -70,14 +72,15 @@ namespace TextMood
 			{
 				IsRefreshing = false;
 			}
-        }
-		void SetTextResultsListBackgroundColor(List<ITextMoodModel> textMoodModelList)
+		}
+
+		void SetTextResultsListBackgroundColor(IList<ITextMoodModel> textMoodModelList)
 		{
 			var averageSentiment = (double)TextModelServices.GetAverageSentimentScore(textMoodModelList);
-            if (averageSentiment < 0)
-                BackgroundColor = default;
-            else
-                BackgroundColor = Color.FromRgba(1 - averageSentiment, averageSentiment, 0, 0.5);
+			if (averageSentiment < 0 || averageSentiment > 1)
+				BackgroundColor = default;
+			else
+				BackgroundColor = Color.FromRgba(1 - averageSentiment, averageSentiment, 0, 0.5);
 		}
 
 		void OnErrorTriggered(string message) => ErrorTriggered?.Invoke(this, message);
