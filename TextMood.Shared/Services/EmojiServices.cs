0750634a21206376d1e8125c10f1b221b98b5cdb
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-28 17:37:22 -0700

    Added Emoji Support

---
+++ TextMood.Shared/Services/EmojiServices.cs
@@ -0,0 +1,22 @@
+﻿namespace TextMood.Shared
+{
+    public static class EmojiServices
+    {
+        public static string GetEmoji(float? sentimentScore)
+        {
+            switch (sentimentScore)
+            {
+                case float number when (number >= 0 && number < 0.4):
+                    return EmojiConstants.SadFaceEmoji;
+                case float number when (number >= 0.4 && number <= 0.6):
+                    return EmojiConstants.NeutralFaceEmoji;
+                case float number when (number > 0.6):
+                    return EmojiConstants.HappyFaceEmoji;
+                case null:
+                    return EmojiConstants.BlankFaceEmoji;
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
