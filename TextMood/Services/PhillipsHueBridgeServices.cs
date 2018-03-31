e92f7a0c356304d5ec8c74092a07547a1ee38d5f
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-31 14:00:02 -0700

    Added AutoDetect Feature to Setup Page

--- TextMood/Services/PhillipsHueBridgeServices.cs
+++ TextMood/Services/PhillipsHueBridgeServices.cs
@@ -1,25 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 using Plugin.Settings;
 using Plugin.Connectivity;
 
+using Xamarin.Forms;
+
 namespace TextMood
 {
 	abstract class PhillipsHueBridgeServices : BaseHttpClientService
 	{
+		#region Properties
 		public static string PhillipsHueBridgeIPAddress
 		{
 			get => CrossSettings.Current.GetValueOrDefault(nameof(PhillipsHueBridgeIPAddress), string.Empty);
 			set => CrossSettings.Current.AddOrUpdateValue(nameof(PhillipsHueBridgeIPAddress), value);
 		}
 
+		public static string PhillipsHueBridgeID
+		{
+			get => CrossSettings.Current.GetValueOrDefault(nameof(PhillipsHueBridgeID), string.Empty);
+			set => CrossSettings.Current.AddOrUpdateValue(nameof(PhillipsHueBridgeID), value);
+		}
+
+		public static string PhillipsBridgeUserName
+		{
+			get => CrossSettings.Current.GetValueOrDefault(nameof(PhillipsBridgeUserName), string.Empty);
+			set => CrossSettings.Current.AddOrUpdateValue(nameof(PhillipsBridgeUserName), value);
+		}
+		#endregion
+
+		#region Methods
+		public static async ValueTask<List<PhillipsHueUserNameDiscoveryModel>> AutoDetectUserName()
+		{
+			var isBridgeReachable = await IsBridgeReachable().ConfigureAwait(false);
+			if (!isBridgeReachable)
+				throw new Exception(GetBridgeNotFoundErrorMessage());
+
+			var deviceType = new PhillipsHueDeviceTypeModel { Devicetype = string.Empty };
+			return await PostObjectToAPI<List<PhillipsHueUserNameDiscoveryModel>, PhillipsHueDeviceTypeModel>($"http://{PhillipsHueBridgeIPAddress}/api", deviceType).ConfigureAwait(false);
+		}
+
+		public static Task<List<PhillipsHueBridgeDiscoveryModel>> AutoDetectBridges() =>
+			GetFromAPI<List<PhillipsHueBridgeDiscoveryModel>>("https://www.meethue.com/api/nupnp");
+
 		public static async ValueTask<HttpResponseMessage> UpdateLightBulbColor(int hue)
 		{
 			var isBridgeReachable = await IsBridgeReachable().ConfigureAwait(false);
 			if (!isBridgeReachable)
-				return default;
+				throw new Exception(GetBridgeNotFoundErrorMessage());
 
 			var hueRequest = new PhillipsHueRequestModel
 			{
@@ -29,7 +60,7 @@ namespace TextMood
 				Brightness = 255
 			};
 
-			return await PutObjectToAPI($"http://{PhillipsHueBridgeIPAddress}/api/5pE71iepzEeuKQY1SSfwuiATTfih3dy0YZDAwhCh/lights/1/state", hueRequest).ConfigureAwait(false);
+			return await PutObjectToAPI($"http://{PhillipsHueBridgeIPAddress}/api/{PhillipsBridgeUserName}/lights/1/state", hueRequest).ConfigureAwait(false);
 		}
 
 		static async ValueTask<bool> IsBridgeReachable()
@@ -44,5 +75,25 @@ namespace TextMood
 				return false;
 			}
 		}
+
+		static string GetBridgeNotFoundErrorMessage()
+		{
+			const string bridgeNotFoundError = "Bridge Not Found.";
+
+			switch (Device.Idiom)
+			{
+				case TargetIdiom.Desktop:
+					return $"{bridgeNotFoundError} Ensure this computer and the Phillips Bridge are connected to the same local network";
+				case TargetIdiom.Phone:
+					return $"{bridgeNotFoundError} Ensure this phone and the Phillips Bridge are connected to the same local network";
+				case TargetIdiom.Tablet:
+					return $"{bridgeNotFoundError} Ensure this tablet and the Phillips Bridge are connected to the same local network";
+				case TargetIdiom.TV:
+					return $"{bridgeNotFoundError} Ensure this TV and the Phillips Bridge are connected to the same local network";
+				default:
+					return $"{bridgeNotFoundError} Ensure this app and the Phillips Bridge are connected to the same local network";
+			}
+		}
+		#endregion
 	}
 }
