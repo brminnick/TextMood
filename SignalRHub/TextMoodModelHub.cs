c3b6fc6507e1b463e1b167ed30fc2c65c791c9a1
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-31 15:48:47 -0700

    Added Signal R Backend

---
+++ SignalRHub/TextMoodModelHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+
+using TextMood.Backend.Common;
+
+namespace SignalRHub
+{
+    public class TextMoodModelHub : Hub
+    {
+        public void Hello(TextMoodModel textMoodModel)
+        {
+            Clients.All.broadcastMessage(textMoodModel);
+        }
+    }
+}
