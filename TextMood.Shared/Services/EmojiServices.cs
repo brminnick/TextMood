68f19a505e398d58c17147aa456ac0041f284d8e
Brandon Minnick <brminnick@users.noreply.github.com>
2018-04-30 17:44:53 -0700

    Updated TextAPI SDK

--- TextMood.Shared/Services/EmojiServices.cs
+++ TextMood.Shared/Services/EmojiServices.cs
@@ -2,15 +2,15 @@
 {
     public static class EmojiServices
     {
-        public static string GetEmoji(float? sentimentScore)
+        public static string GetEmoji(double? sentimentScore)
         {
             switch (sentimentScore)
             {
-                case float number when (number >= 0 && number < 0.4):
+                case double number when (number >= 0 && number < 0.4):
                     return EmojiConstants.SadFaceEmoji;
-                case float number when (number >= 0.4 && number <= 0.6):
+                case double number when (number >= 0.4 && number <= 0.6):
                     return EmojiConstants.NeutralFaceEmoji;
-                case float number when (number > 0.6):
+                case double number when (number > 0.6):
                     return EmojiConstants.HappyFaceEmoji;
                 case null:
                     return EmojiConstants.BlankFaceEmoji;
