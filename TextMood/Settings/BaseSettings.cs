313a2bdab3114495e340c80f5b0c31c6c0d696e6
Brandon Minnick <brminnick@users.noreply.github.com>
2018-06-17 08:13:00 +0200

    Added Xamarin Essentials

--- TextMood/Settings/BaseSettings.cs
+++ TextMood/Settings/BaseSettings.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Runtime.CompilerServices;
 
-using Plugin.Settings;
-using System;
+using Xamarin.Essentials;
 
 namespace TextMood
 {
@@ -11,19 +11,19 @@ namespace TextMood
 	{
 		#region Methods
 		protected static string GetSetting(ref string backingStore, string defaultValue = "", [CallerMemberName]string propertyName = "") =>
-			backingStore ?? (backingStore = CrossSettings.Current.GetValueOrDefault(propertyName, defaultValue));
+			backingStore ?? (backingStore = Preferences.Get(propertyName, defaultValue));
 
 		protected static bool GetSetting(ref bool backingStore, bool defaultValue = default, [CallerMemberName]string propertyName = "") =>
-		    backingStore.Equals(default(bool)) ? (backingStore = CrossSettings.Current.GetValueOrDefault(propertyName, defaultValue)) : backingStore;
+		    backingStore.Equals(default(bool)) ? (backingStore = Preferences.Get(propertyName, defaultValue)) : backingStore;
 
 		protected static int GetSetting(ref int backingStore, int defaultValue = default, [CallerMemberName]string propertyName = "") =>
-			backingStore.Equals(default(int)) ? (backingStore = CrossSettings.Current.GetValueOrDefault(propertyName, defaultValue)) : backingStore;
+			backingStore.Equals(default(int)) ? (backingStore = Preferences.Get(propertyName, defaultValue)) : backingStore;
 
 		protected static DateTimeOffset GetSetting(ref DateTimeOffset backingStore, DateTimeOffset defaultValue = default, [CallerMemberName]string propertyName = "") =>
-			backingStore.Equals(default(DateTimeOffset)) ? (backingStore = new DateTimeOffset(CrossSettings.Current.GetValueOrDefault(propertyName, defaultValue.UtcDateTime))) : backingStore;
+			backingStore.Equals(default(DateTimeOffset)) ? (backingStore = new DateTimeOffset(Preferences.Get(propertyName, defaultValue.UtcDateTime))) : backingStore;
 
 		protected static IPAddress GetSetting(ref IPAddress backingStore, [CallerMemberName]string propertyName = "") =>
-			backingStore ?? (backingStore = IPAddress.Parse(CrossSettings.Current.GetValueOrDefault(propertyName, "0.0.0.0")));
+			backingStore ?? (backingStore = IPAddress.Parse(Preferences.Get(propertyName, "0.0.0.0")));
 
 		protected static void SetSetting(ref string backingStore, string value, [CallerMemberName]string propertyName = "")
 		{
@@ -32,7 +32,7 @@ namespace TextMood
 
 			backingStore = value;
 
-			CrossSettings.Current.AddOrUpdateValue(propertyName, value);
+			Preferences.Set(propertyName, value);
 		}
 
 		protected static void SetSetting(ref bool backingStore, bool value, [CallerMemberName]string propertyName = "")
@@ -42,7 +42,7 @@ namespace TextMood
 
             backingStore = value;
 
-            CrossSettings.Current.AddOrUpdateValue(propertyName, value);
+            Preferences.Set(propertyName, value);
         }
 
 		protected static void SetSetting(ref int backingStore, int value, [CallerMemberName]string propertyName = "")
@@ -52,7 +52,7 @@ namespace TextMood
 
             backingStore = value;
 
-            CrossSettings.Current.AddOrUpdateValue(propertyName, value);
+            Preferences.Set(propertyName, value);
         }
 
 		protected static void SetSetting(ref DateTimeOffset backingStore, DateTimeOffset value, [CallerMemberName]string propertyName = "")
@@ -62,7 +62,7 @@ namespace TextMood
 
 			backingStore = value;
 
-			CrossSettings.Current.AddOrUpdateValue(propertyName, value.UtcDateTime);
+			Preferences.Set(propertyName, value.UtcDateTime);
 		}
 
 		protected static void SetSetting(ref IPAddress backingStore, IPAddress value, [CallerMemberName]string propertyName = "")
@@ -72,7 +72,7 @@ namespace TextMood
 
 			backingStore = value;
 
-			CrossSettings.Current.AddOrUpdateValue(propertyName, value.ToString());
+			Preferences.Set(propertyName, value.ToString());
 		}
 		#endregion
 	}
