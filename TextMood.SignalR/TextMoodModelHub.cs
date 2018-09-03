075ae958c574334ffaa2728f31f8e1abc1a872f2
Brandon Minnick <brminnick@users.noreply.github.com>
2018-09-03 16:08:27 -0700

    Added .NET Core Signal R Project

---
+++ TextMood.SignalR/TextMoodModelHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+using TextMood.Backend.Common;
+
+namespace SignalRHub
+{
+	public class TextMoodModelHub : Hub
+	{
+		public void SendNewTextMoodModel(TextMoodModel textMoodModel) => Clients.Others.SendNewTextMoodModel(textMoodModel);
+	}
+}
