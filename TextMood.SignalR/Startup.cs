cfc9142dc0b91e1b55b5a2e2411ed2298facc501
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2018-11-12 12:32:46 -0800

    Formatted Code

--- TextMood.SignalR/Startup.cs
+++ TextMood.SignalR/Startup.cs
@@ -1,28 +1,21 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+
 using TextMood.Shared;
 
 namespace TextMood.SignalR
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSignalR();
-        }
+        public void ConfigureServices(IServiceCollection services) => services.AddSignalR();
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
-            app.UseSignalR(route =>
-            {
-                route.MapHub<TextMoodModelHub>($"/{SignalRConstants.TextMoodModelHubName}");
-            });
+            app.UseSignalR(route => route.MapHub<TextMoodModelHub>($"/{SignalRConstants.TextMoodModelHubName}"));
         }
     }
 }
