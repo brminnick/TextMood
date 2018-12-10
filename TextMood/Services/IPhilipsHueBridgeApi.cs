c8cb84e9eb9da5bf842dd7f87d524b97c484fb52
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2018-12-10 14:15:58 -0800

    Added IPhilipsHueBridgeApi

---
+++ TextMood/Services/IPhilipsHueBridgeApi.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Refit;
+
+namespace TextMood
+{
+    public interface IPhilipsHueBridgeApi
+    {
+        [Get("/api/{philipsHueBridgeUsername}/lights")]
+        Task<JObject> GetNumberOfLights(string philipsHueBridgeUsername);
+
+        [Post("/api")]
+        Task<List<PhilipsHueUsernameDiscoveryModel>> AutoDetectUsername([Body]PhilipsHueDeviceTypeModel device);
+
+        [Put("/api/{philipsHueBridgeUsername}/lights/{lightNumber}/state")]
+        Task UpdateLightBulbColor([Body]LightModel light, string philipsHueBridgeUsername, int lightNumber);
+
+        [Get("/")]
+        Task<HttpResponseMessage> GetPhilipsHueBridgeResponse();
+    }
+}
