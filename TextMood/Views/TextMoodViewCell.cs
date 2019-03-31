c99579346204b167ff7fd56e25232b00a2ee5e95
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-03-31 16:18:36 -0400

    Fixed Background Color

--- TextMood/Views/TextMoodViewCell.cs
+++ TextMood/Views/TextMoodViewCell.cs
@@ -4,57 +4,54 @@ using TextMood.Shared;
 
 namespace TextMood
 {
-	public class TextMoodViewCell : ViewCell
-	{
-		#region Constant Fields
-		readonly Label _titleLabel, _descriptionLabel;
-		#endregion
+    public class TextMoodViewCell : ViewCell
+    {
+        #region Constant Fields
+        readonly Label _titleLabel, _descriptionLabel;
+        #endregion
 
-		#region Constructors
-		public TextMoodViewCell()
-		{
-			_titleLabel = new Label
-			{
-				FontAttributes = FontAttributes.Bold,
-			};
+        #region Constructors
+        public TextMoodViewCell()
+        {
+            _titleLabel = new Label { FontAttributes = FontAttributes.Bold };
 
-			_descriptionLabel = new Label();
+            _descriptionLabel = new Label();
 
-			var gridLayout = new Grid
-			{
-				Padding = new Thickness(20, 5),
-				RowSpacing = 2,
-				ColumnSpacing = 10,
+            var gridLayout = new Grid
+            {
+                Padding = new Thickness(20, 5),
+                RowSpacing = 2,
+                ColumnSpacing = 10,
 
-				RowDefinitions = {
-					new RowDefinition{ Height = new GridLength(0, GridUnitType.Auto) },
-					new RowDefinition{ Height = new GridLength(0, GridUnitType.Auto) }
+                RowDefinitions = {
+                    new RowDefinition{ Height = new GridLength(0, GridUnitType.Auto) },
+                    new RowDefinition{ Height = new GridLength(0, GridUnitType.Auto) }
 
-				},
-				ColumnDefinitions = {
-					new ColumnDefinition{ Width = new GridLength(0, GridUnitType.Auto) }
-				}
-			};
+                },
+                ColumnDefinitions = {
+                    new ColumnDefinition{ Width = new GridLength(0, GridUnitType.Auto) }
+                }
+            };
 
-			gridLayout.Children.Add(_titleLabel, 0, 0);
-			gridLayout.Children.Add(_descriptionLabel, 0, 1);
+            gridLayout.Children.Add(_titleLabel, 0, 0);
+            gridLayout.Children.Add(_descriptionLabel, 0, 1);
 
-			View = gridLayout;
-		}
-		#endregion
+            View = gridLayout;
+        }
+        #endregion
 
-		#region Methods
-		protected override void OnBindingContextChanged()
-		{
-			base.OnBindingContextChanged();
+        #region Methods
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
 
-			var textModel = BindingContext as TextMoodModel;
+            var textModel = BindingContext as TextMoodModel;
 
-			var emoji = EmojiServices.GetEmoji(textModel.SentimentScore);
+            var emoji = EmojiServices.GetEmoji(textModel.SentimentScore);
 
-			_titleLabel.Text = textModel.Text;
-			_descriptionLabel.Text = $"{emoji} {textModel.CreatedAt.ToLocalTime().ToString("g")}";
-		}
-		#endregion
-	}
+            _titleLabel.Text = textModel.Text;
+            _descriptionLabel.Text = $"{emoji} {textModel.CreatedAt.ToLocalTime().ToString("g")}";
+        }
+        #endregion
+    }
 }
