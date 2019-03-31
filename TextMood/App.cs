e5d1ecc90f92ddb844b7a04ceae64ffef5981eac
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-03-31 17:07:22 -0400

    Updated ListView.RefreshControlColor

--- TextMood/App.cs
+++ TextMood/App.cs
@@ -1,5 +1,4 @@
-﻿using Xamarin.Forms;
-using Xamarin.Forms.PlatformConfiguration;
+﻿using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace TextMood
